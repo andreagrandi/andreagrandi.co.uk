efee09d3505d5ef92c4a54de2d9b7acd586a0605
Andrea Grandi <a.grandi@gmail.com>
2017-06-27 22:31:08 +0100

    Fix social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,8 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/andreagrandi'),
+          ('github', 'https://github.com/andreagrandi'),)
 
 DEFAULT_PAGINATION = 10
 
